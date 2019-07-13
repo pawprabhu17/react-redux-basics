8e3d5f9d96f7aa50b13ca70559930e5c3f0a772b
pawprabhu17 <pawprabhu17@gmail.com>
2019-07-13 16:35:58 +0530

    Initial commit

---
+++ myapp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Proteas from './proteas';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My First React app</h1>
+      <p>Welcome :)</p>
+      <Proteas/>
+    </div>
+  );
+}
+
+export default App;
---
+++ myapp/src/proteas.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Proteas() {
+    return(
+        <div className="Proteas">
+            <div>Name: ABD</div>
+            <div>Age:35</div>
+            <div>Style:Batsman</div>
+        </div>
+    )
+}
+
+export default Proteas;
